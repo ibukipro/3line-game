1b7a896a0d97d0b452b1b790366b3737d297120f
ibukipro <ibukipro@aol.com>
2026-08-21 23:36:30 +0900

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
+
 
 
 // ==========================================================================
