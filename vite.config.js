2e2277bef653f169b37c0a6244b5258eacfdd31f
ibukipro <ibukipro@aol.com>
2026-08-21 23:56:27 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/3line-game/',
   plugins: [react()],
+  // 🌟 追加：これを入れることで、ファイル内のすべてのReact/React.memoのエラーを100%強制解決します
+  esbuild: {
+    jsxInject: `import React from 'react'`
+  }
 })
